826e6fd32d4aea7724aa4713bb7324eb9f43d74f
desongyu <desong.e.yu@gmail.com>
2016-12-30 17:03:19 -0500

    vote and next

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -1,4 +1,4 @@
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 export function setEntries(state, entries) {
 	// pass the given entries into the List constructor 
@@ -6,3 +6,36 @@ export function setEntries(state, entries) {
 	// array (or actually anything iterable)
   return state.set('entries', List(entries));
 }
+
+function getWinners(vote) {
+  if (!vote) return [];
+  const [a, b] = vote.get('pair');
+  const aVotes = vote.getIn(['tally', a], 0);
+  const bVotes = vote.getIn(['tally', b], 0);
+  if      (aVotes > bVotes)  return [a];
+  else if (aVotes < bVotes)  return [b];
+  else                       return [a, b];
+}
+
+export function next(state) {
+  const entries = state.get('entries')
+                       .concat(getWinners(state.get('vote')));
+  return state.merge({
+    vote: Map({pair: entries.take(2)}),
+    entries: entries.skip(2)
+  });
+}
+
+//reach into the nested data structure path 
+//['vote', 'tally', 'Trainspotting'], and apply 
+//this function there. If there are keys missing 
+//along the path, create new Maps in their place. 
+//If the value at the end is missing, initialize it with 0
+export function vote(state, entry) {
+	return state.updateIn(
+			['vote','tally',entry],
+			0,
+			tally => tally + 1
+		);
+}
+
