cee8323f96105e9734b33b4d1fb80198def4c944
desongyu <desong.e.yu@gmail.com>
2017-01-04 09:22:49 -0500

    vote w pure functions

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -20,10 +20,17 @@ function getWinners(vote) {
 export function next(state) {
   const entries = state.get('entries')
                        .concat(getWinners(state.get('vote')));
-  return state.merge({
-    vote: Map({pair: entries.take(2)}),
-    entries: entries.skip(2)
-  });
+
+  if (entries.size === 1)                        {
+  	return state.remove('vote')
+  				.remove('entries')
+  				.set('winner', entries.first());
+  } else {
+	  return state.merge({
+	    vote: Map({pair: entries.take(2)}),
+	    entries: entries.skip(2)
+	  });
+  }
 }
 
 //reach into the nested data structure path 
